119fde28140710a9a7a59c7fc7ee0e593b1de319
Stuart Kent <stuart@stuartkent.co.nz>
2020-03-22 11:12:54 +1300

    newgame

---
+++ contagion/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1584828084,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/group-sheet0.png",
+		"images/group3-sheet0.png",
+		"images/group4-sheet0.png",
+		"images/group5-sheet0.png",
+		"images/rectangle-sheet0.png",
+		"images/inviswall-sheet0.png",
+		"images/rectangle2-sheet0.png",
+		"images/particles.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
